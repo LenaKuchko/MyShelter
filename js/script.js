62af3c56cf9f042f294e27dfec717ed49a58d609
Illia <ikuchko@gmail.com>
2017-02-08 09:23:04 -0800

    add gallery

--- js/script.js
+++ js/script.js
@@ -2,18 +2,63 @@
 
 $(document).ready(function() {
   // debugger;
-  console.log("Hello")
-  var i = 1;
   console.log(JSON.parse(cats));
-  var json = JSON.parse(cats)
+  var petCats = JSON.parse(cats)
   // debugger;
-  $('#cat1').append('<div class="col-md-4">' +
-            '<div class="thumbnail">' +
-            '<img class="img-responsive" src="cats/Leo.jpg" alt="Leo">' +
-            '<div class="caption">' +
-              json[1].Name + '<br>10<br>male' +
-            '</div>' +
-            '</div>'
-  );
-  console.log();
-});
+  // $('#cat1').append('<div class="col-md-4">' +
+  //           '<div class="thumbnail">' +
+  //           '<img class="img-responsive" src="cats/Leo.jpg" alt="Leo">' +
+  //           '<div class="caption">' +
+  //             json[1].Name + '<br>10<br>male' +
+  //           '</div>' +
+  //           '</div>'
+  // );
+  // console.log();
+  debugger;
+  var catcounter=0;
+  for (var i = 0; i < (petCats.length)/3; i++) {
+    $('#viewCats').append('<div class="row" id="rowCat'+ i +'"> '+'</div>' );
+
+    if ((i+1)>(petCats.length/3))
+    {
+      for (var y = 0; y < petCats.length%3; y++) {
+        $('#rowCat'+i).append('<div class="col-md-4">' +
+                      '<div class="thumbnail"> ' +
+                      '<a data-toggle="modal" data-target="#myModal">'+
+                      '<img class="img-responsive" src="cats/' +
+
+                      petCats[catcounter].Name + petCats[catcounter].id +
+                      '.jpg" alt="' + petCats[catcounter].Name + '">' +
+                      '</a>' +
+                      '<div class="caption">' +
+                      petCats[catcounter].Name + ', ' + petCats[catcounter].Age +
+                      ', ' + petCats[catcounter].Sex + '<br>' +
+                      'Favorite food: ' +
+                      '</div>' +
+                      '</div>' +
+                      '</div>'
+                    );
+         console.log(petCats[catcounter].Name);
+         catcounter++;}
+    }
+    else {
+        for (var x = 0; x < 3; x++) {
+        $('#rowCat'+i).append('<div class="col-md-4">' +
+                      '<div class="thumbnail"> ' +
+                      '<img class="img-responsive" src="cats/'
+                      + petCats[catcounter].Name + petCats[catcounter].id +
+                      '.jpg" alt="' + petCats[catcounter].Name + '">' +
+                      '<div class="caption">' +
+                      petCats[catcounter].Name + ', ' + petCats[catcounter].Age +
+                      ', ' + petCats[catcounter].Sex + '<br>' +
+                      'Favorite food: ' +
+                      '</div>' +
+                      '</div>' +
+                      '</div>'
+                    );
+
+          console.log(petCats[catcounter].Name);
+            catcounter++;}
+    }
+  }
+})
