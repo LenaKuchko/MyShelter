a06e47e8e405580d1d1b01bcf4250003a90d97e1
Illia <ikuchko@gmail.com>
2017-02-16 11:51:09 -0800

    add addresses

--- js/shelterScript.js
+++ js/shelterScript.js
@@ -1,11 +1,40 @@
+
+  var zoo={lat: 45.510037, lng: -122.715846};
+  var map;
+  var marker;
+
+  //document.getElementById("Oregon zoo").onclick = showLocation("Oregon zoo");
+
 function initMap() {
-  var uluru = {lat: 49.6021346, lng: 34.487199};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+  // debugger;
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: zoo,
+    mapTypeId: 'terrain'
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
+
+  marker = new google.maps.Marker({
+    position: zoo,
     map: map
   });
+}
+function showLocation(Name){
+  debugger;
+  var addresses=JSON.parse(address);
+
+  for (var i = 0; i < addresses.length; i++) {
+    if (Name==addresses[i].Name) {
+      var shelter = new google.maps.LatLng ({ lat: parseFloat(addresses[i].lat), lng: parseFloat(addresses[i].lng)});
+      marker.setPosition(shelter);
+      marker.setMap(map);
+      map.setCenter(shelter);
+      break;
+    }
+  }
+  // var shelter = new google.maps.LatLng ({ lat: parseFloat(addresses[i].lat), lng: parseFloat(addresses[i].lng)});
+  // marker.setPosition(shelter);
+  // marker.setMap(map);
+  // map.setCenter(shelter);
+
 }
